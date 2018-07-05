44e8907d53c8667f2cd9bdfb6db7cc71b369e452
yasirkhanxyz <yasirkhanxyz@gmail.com>
2018-07-05 21:13:49 +0530

    Added the Class conversion changes

--- assignment-qa-automation-crossover/e2e/test-suites/regression.e2e-spec.ts
+++ assignment-qa-automation-crossover/e2e/test-suites/regression.e2e-spec.ts
@@ -1,9 +1,11 @@
 import {LoginPage} from '../page-objects/pages/login/login.po';
 import {LoginPageHelper} from '../page-objects/pages/login/login-page.helper';
-import {browser, By, element, ExpectedConditions} from 'protractor';
+import {browser,ExpectedConditions} from 'protractor';
+
 
 describe('G mail suite', () => {
     let loginPageHelper: LoginPageHelper;
+    
 
     beforeEach(() => {
         loginPageHelper = new LoginPageHelper();
@@ -12,25 +14,37 @@ describe('G mail suite', () => {
     it('Send email', async () => {
         const user1 = browser.params.user1;
         await loginPageHelper.goToPage();
+        const X1 = await loginPageHelper.SignInlabel();
+        console.log(X1 + 'Page Loaded Successfully');
         await LoginPage.username.sendKeys(user1.username);
 
-        await element(By.id('identifierNext')).click();
+        await LoginPage.UserNextButton.click();
         await browser.wait(ExpectedConditions.visibilityOf(LoginPage.password));
 
         await LoginPage.password.sendKeys(user1.password);
 
         await browser.sleep(5000);
         await LoginPage.passwordNextButton.click();
-        await browser.wait(ExpectedConditions.visibilityOf(element(By.xpath(`//*[@role='button' and (.)='COMPOSE']`))));
+        await browser.sleep(50000);
+        const X2 = await loginPageHelper.LoggedIn();
+        if (X2 == true)
+        {
+            console.log("User Logged In Succesfully");
+        }
+        else {
+            console.log("User Failed To log in or Element Failed to Locate");
+        }
+        await browser.wait(ExpectedConditions.visibilityOf(LoginPage.ComposeButton));
+        await LoginPage.ComposeButton.click();
+        await browser.sleep(3000);
+        await LoginPage.SendToTextBox.clear();
+        await LoginPage.SendToTextBox.sendKeys(user1.username);
+        await LoginPage.SubjectTextBox.clear();
+        await LoginPage.SubjectTextBox.sendKeys('Yasir sent you this with this subject');
+        await LoginPage.BodyTextBox.click();
+        await LoginPage.BodyTextBox.sendKeys('Yasir sent you with this Body');
+        await LoginPage.SentButton.click();
 
-        await element(By.xpath(`//*[@role='button' and (.)='COMPOSE']`)).click();
-
-        await browser.sleep(15000);
-        await element(By.css('[name="to"]')).clear();
-        await element(By.css('[name="to"]')).sendKeys('secondUser@gmail.com');
-
-        await loginPageHelper.click(element(By.xpath('//*[@role="button" and text()="Send"]')));
-
-        await browser.sleep(15000);
+        await browser.sleep(5000);
     });
 });
